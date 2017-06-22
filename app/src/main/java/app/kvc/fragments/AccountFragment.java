2ac8f0159f36ee176262d0712ad8ee285cd0439f
msoftteam <surasak.ict.msu@gmail.com>
2017-06-22 16:32:36 +0700

    add home screen

---
+++ app/src/main/java/app/kvc/fragments/AccountFragment.java
@@ -0,0 +1,25 @@
+package app.kvc.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.kvc.kvc_ict.R;
+
+/**
+ * Created by DSOFT on 6/22/2017 AD.
+ */
+
+public class AccountFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_account, container, false);
+
+        return rootView;
+
+    }
+}
--- app/src/main/java/app/kvc/kvc_ict/MainActivity.java
+++ app/src/main/java/app/kvc/kvc_ict/MainActivity.java
@@ -1,13 +1,77 @@
 package app.kvc.kvc_ict;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import app.kvc.fragments.AccountFragment;
+import app.kvc.fragments.MainFragment;
+
+import static app.kvc.kvc_ict.R.id.drawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private NavigationView nv;
+
+    //private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        //setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final FragmentManager fm = getFragmentManager();
+        fm.beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
+
+        nv = (NavigationView) findViewById(R.id.navigation_view);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                if (id == R.id.nav_home) {
+                    fm.beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
+                } else if (id == R.id.nav_account) {
+//                    Toast.makeText(getApplicationContext(), "account click", Toast.LENGTH_SHORT).show();
+                    fm.beginTransaction().replace(R.id.content_frame, new AccountFragment()).commit();
+                } /*else if (id == R.id.nav_setting) {
+                    Toast.makeText(getApplicationContext(), "setting click", Toast.LENGTH_SHORT).show();
+                }*/ else if (id == R.id.nav_logout) {
+                    Toast.makeText(getApplicationContext(), "Logout click", Toast.LENGTH_SHORT).show();
+                }
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
